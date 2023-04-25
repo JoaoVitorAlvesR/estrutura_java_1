65ec9729d53043710ff4fb7f7eb57d76eb2386d4
Jhonny Mezzari <jhonnybif@gmail.com>
2023-04-25 20:31:31 -0300

    bkp banco

---
+++ Sistema/backupBanco.bat
@@ -0,0 +1,8 @@
+set path_old=%path%
+set PGPASSWORD=%2
+set path=C:\Program Files\PostgreSQL\15\bin;%path%
+start pg_dump -h localhost -p 5432 -U %1 -w -F c -b -v -f %3 sistema
+set path=%path_old%
+set path_old=
+set PGPASSWORD=
+exit
