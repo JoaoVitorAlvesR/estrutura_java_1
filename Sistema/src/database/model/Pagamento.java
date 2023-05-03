7896e3efa4f4b649eab4fb3f8a71ce2a828e55b4
Laboratorio XXIA - 201 <lab201a@lab.unesc.net>
2023-05-02 21:18:28 -0300

    add payment

---
+++ Sistema/src/database/model/Pagamento.java
@@ -0,0 +1,76 @@
+package database.model;
+
+import java.sql.Timestamp;
+
+import database.StatusPagamento;
+
+public class Pagamento {
+	private Integer codigoMatricula;
+	private Timestamp dataVencimento;
+	private Double valor;
+	private Timestamp dataPagamento;
+	private Timestamp dataCancelamento;
+	private StatusPagamento status;
+	
+	public Pagamento(Integer codigoMatricula, Timestamp dataVencimento, Timestamp dataPagamento,
+			Timestamp dataCancelamento, Double valor, StatusPagamento status) {
+		this.codigoMatricula = codigoMatricula;
+		this.dataVencimento = dataVencimento;
+		this.valor = valor;
+		this.dataPagamento = dataPagamento;
+		this.dataCancelamento = dataCancelamento;
+		this.status = status;
+	}
+	
+	public Pagamento() {
+		
+	}
+
+	public Integer getCodigoMatricula() {
+		return codigoMatricula;
+	}
+
+	public void setCodigoMatricula(Integer codigoMatricula) {
+		this.codigoMatricula = codigoMatricula;
+	}
+
+	public Timestamp getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Timestamp dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Timestamp getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Timestamp dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Timestamp getDataCancelamento() {
+		return dataCancelamento;
+	}
+
+	public void setDataCancelamento(Timestamp dataCancelamento) {
+		this.dataCancelamento = dataCancelamento;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public StatusPagamento getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPagamento status) {
+		this.status = status;
+	}
+}
